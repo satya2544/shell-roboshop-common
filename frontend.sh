e9ca2c0a4debb6e86116d3fd83d3afad3101887a
satya2544 <koppolu.satya@gmail.com>
2026-08-16 14:49:23 +0100

    some message

--- frontend.sh
+++ frontend.sh
@@ -43,21 +43,17 @@ systemctl enable nginx &>>"$LOG_FILE"
 VALIDATE $? "Enabling Nginx"
 
 rm -rf /usr/share/nginx/html/*
-
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip &>>"$LOG_FILE"
+cd /usr/share/nginx/html
+unzip -o /tmp/frontend.zip &>>"$LOG_FILE"
 VALIDATE $? "Downloading frontend"
 
-cd /usr/share/nginx/html
-VALIDATE $? "Changing to frontend directory"
-
-unzip -o /tmp/frontend.zip &>>"$LOG_FILE"
-VALIDATE $? "Extracting frontend"
-
+rm -rf /etc/nginx/nginx.conf
 cp "$SCRIPT_DIR/nginx.conf" /etc/nginx/nginx.conf
 VALIDATE $? "Copying Nginx configuration"
 
-nginx -t &>>"$LOG_FILE"
-VALIDATE $? "Testing Nginx configuration"
+# nginx -t &>>"$LOG_FILE"
+# VALIDATE $? "Testing Nginx configuration"
 
 systemctl restart nginx &>>"$LOG_FILE"
 VALIDATE $? "Restarting Nginx"
